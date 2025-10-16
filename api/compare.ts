c66169cfdae62cc922cb8d4f7a2cca4fc1c218d9
Branimir Sabljic <bsabljic@gmail.com>
2025-10-16 18:31:52 +0200

    add: Node/TS /api/compare endpoint

---
+++ api/compare.ts
@@ -0,0 +1,10 @@
+import type { VercelRequest, VercelResponse } from '@vercel/node';
+
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+  res.status(200).json({
+    ok: true,
+    runtime: 'node',
+    endpoint: '/api/compare',
+    message: 'Node/TS function alive'
+  });
+}
